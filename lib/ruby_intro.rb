d9185af05e10b207b83c27c339914bbfd1feea0c
EC2 Default User <ec2-user@ip-172-31-25-118.us-east-2.compute.internal>
2019-01-29 19:06:41 +0000

    hw1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,52 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+   arr.inject(0,:+)
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(a)
+	return 0 if a.empty?
+	return a[0] if a.length == 1
+	ar = a.sort
+	ar[-1] + ar[-2]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+   return false if arr.empty? && n.zero?
+  arr.combination(2).any? {|a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+    !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
+    #!!(s[0] !~ /[aeiou]+/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s == "0"
+  /^[10]*00$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+	attr_accessor :price
+
+	def initialize(isbn, price)
+	  raise ArgumentError,"Illegal argument" if isbn.empty?  or price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+	  sprintf("$%2.2f", @price)
+	end
+  
 end
